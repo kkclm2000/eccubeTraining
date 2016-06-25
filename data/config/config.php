9f69b189ddcf0906789398a8c7fcf248cc3881e1
kkanda@three-waves.com <kkanda@three-waves.com>
2016-06-25 17:10:03 +0900

    岡田さんのECCUBE 2.13.5環境の全コミット

---
+++ data/config/config.php
@@ -0,0 +1,22 @@
+<?php
+define('ECCUBE_INSTALL', 'ON');
+define('HTTP_URL', 'http://localhost/eccube-okada/html/');
+define('HTTPS_URL', 'http://localhost/eccube-okada/html/');
+define('ROOT_URLPATH', '/eccube-okada/html/');
+define('DOMAIN_NAME', '');
+define('DB_TYPE', 'mysql');
+define('DB_USER', 'eccube_db_user');
+define('DB_PASSWORD', 'okada');
+define('DB_SERVER', '127.0.0.1');
+define('DB_NAME', 'eccube_db');
+define('DB_PORT', '');
+define('ADMIN_DIR', 'okadakanri/');
+define('ADMIN_FORCE_SSL', FALSE);
+define('ADMIN_ALLOW_HOSTS', 'a:0:{}');
+define('AUTH_MAGIC', 'kaechaeuojugoneapiapaesloumiadraegejowru');
+define('PASSWORD_HASH_ALGOS', 'sha256');
+define('MAIL_BACKEND', 'mail');
+define('SMTP_HOST', '');
+define('SMTP_PORT', '');
+define('SMTP_USER', '');
+define('SMTP_PASSWORD', '');
